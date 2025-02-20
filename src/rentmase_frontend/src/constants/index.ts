a8f744b03426f2ac9a67216cdf823a444c0d2543
iamenochchirima <iamenochchirima@gmail.com>
2025-02-20 20:40:03 +0200

    small update

--- src/rentmase_frontend/src/constants/index.ts
+++ src/rentmase_frontend/src/constants/index.ts
@@ -1,7 +1,7 @@
 export { idlFactory as tokenIDL } from "../../../declarations/token/token.did.js";
 export const backendCanisterId = "hqdel-diaaa-aaaai-actvq-cai"
 export const tokenCanisterId = "fr2qs-haaaa-aaaai-actya-cai"
-// export const API_BASE_URL = "https://rentmasebe-production.up.railway.app/api"
+export const API_BASE_URL = "https://rentmasebe-production-764c.up.railway.app/api"
 // export const frontendUrl = "https://55e7x-xyaaa-aaaal-qmzsq-cai.icp0.io"
 // export const defaultAudience = "topups"
 // export const tokensPerReward = 100
@@ -28,7 +28,7 @@ export const cashback =  null
 
 // export const backendCanisterId = "br5f7-7uaaa-aaaaa-qaaca-cai"
 // export const tokenCanisterId = "bkyz2-fmaaa-aaaaa-qaaaq-cai"
-export const API_BASE_URL = "http://localhost:5000/api"
+// export const API_BASE_URL = "http://localhost:5000/api"
 export const frontendUrl = "http://localhost:3000/api"
 export const tokensPerReward = 100
 export const tokenDecimas = 100_000_000
